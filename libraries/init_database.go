64f8c5e0c9adb00d6e3b9fbd6b481071fef2e5c6
wangyl <wangyl@guowei.com>
2024-09-19 20:56:01 +0800

    整理数据库初始化代码, 方便后续使用db类

--- libraries/init_database.go
+++ libraries/init_database.go
@@ -9,22 +9,36 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-func InitDatabase() {
+// 数据库初始化
+func InitDB() (*gorm.DB, error) {
 	dbPath, err := LoadDatabaseConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Check if the database file exists
-	if _, err := os.Stat(dbPath); os.IsNotExist(err) {
-		// If the database file does not exist, create it and initialize the tables
-		db, err := gorm.Open("sqlite3", dbPath)
+	db, err := gorm.Open("sqlite3", dbPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	return db, nil
+}
+
+// 如果文件不存在, 则创建文件, 并且新建数据库
+func CreateNewDBFile() {
+	dbPath, err := LoadDatabaseConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 判断文件是否存在.
+	if _, err = os.Stat(dbPath); os.IsNotExist(err) {
+		db, err := InitDB()
 		if err != nil {
 			log.Fatal(err)
 		}
-		defer db.Close()
-
-		// Auto migrate the tables
+		// 自动创建数据表
 		db.AutoMigrate(&database.WhiteList{}, &database.BlackList{}, &database.Database{}, &database.Interface{}, &database.Params{}, &database.User{}, &database.Token{})
 	}
 }
---
+++ model/whitelist.go
@@ -0,0 +1,16 @@
+package model
+
+/*
+当前项目名称: GoEasyApi
+写管理员操作类.使用中文注释. 并且写上实现过程
+libraries/cache.go 是 缓存类
+libraries/custom_error.go 错误类
+libraries/config.go 配置读取类
+需要实现以下功能:
+	1. 获得白名单列表
+	2. 添加白名单ip
+	3. 删除白名单ip
+	4. 获得黑名单列表
+	5. 添加黑名单ip
+	6. 删除黑名单ip
+*/
