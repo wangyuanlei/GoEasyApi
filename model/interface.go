26164cf52fa81a3f49db3bb57416f7b8726aa9dc
wangyl <wangyl@guowei.com>
2024-09-25 23:26:00 +0800

    增加接口model

---
+++ model/interface.go
@@ -0,0 +1,22 @@
+package model
+
+import "GoEasyApi/database"
+
+type Interface struct{}
+
+// 增加接口
+func (m *Interface) AddInterface(info database.Interface) error {
+
+}
+
+//修改修改
+
+//删除接口
+
+//获得接口详情
+
+//获得接口列表
+
+//保存接口
+
+//保存接口参数
