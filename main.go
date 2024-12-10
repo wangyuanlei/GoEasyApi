5283a9b9ebb38662b790f731bc7ba5391d24b194
wangyl <wangyl@guowei.com>
2024-12-10 22:25:43 +0800

    正式版本v1.0.1

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"GoEasyApi/libraries"
 	"GoEasyApi/model"
 	"GoEasyApi/route"
-	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -22,11 +21,9 @@ func main() {
 	ginServer := gin.Default()
 	// 自定义请求头中设置的用户的真实Ip地址
 	ginServer.TrustedPlatform = "Client-IP"
-	err := ginServer.SetTrustedProxies([]string{"192.168.1.1", "10.0.0.1", "127.0.0.1"})
-	if err != nil {
-		log.Fatal("设置信任的代理失败")
-	}
-	ginServer.StaticFS("/static", http.Dir("./static")) //加载静态文件
+
+	// ginServer.StaticFS("/static", http.Dir("./manger/dist")) //加载静态文件
+	ginServer.StaticFS("/static", http.Dir("./vue/dist")) //加载静态文件
 	ginServer.StaticFile("/favicon.ico", "./static/favicon.ico")
 
 	route.RegisterRoutes(ginServer)
